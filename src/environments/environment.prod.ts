6f3c845704c36f60d1b40c26dac5b65e1e9ed339
thisonewillworkforsure <ben6228@yahoo.com>
2022-10-12 23:09:19 -0400

    first commit 2

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,9 @@
+export const environment = {
+  production: true,
+  withCredentials: true,
+  baseUrl: "http://ec2-54-163-48-95.compute-1.amazonaws.com:8081",
+  headers: {
+    'Content-Type': 'application/json',
+    'Access-Control-Allow-Origin': 'http://mrbucket456.s3.amazonaws.com',
+  },
+};
